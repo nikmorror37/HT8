a8bce6d6895bff8a4cf64726f76768b336090aff
nikmorror37 <nikitamironchuk37@gmail.com>
2023-02-27 20:17:58 +0300

    Commit1
    Task 1 and 2 finish

--- HomeTask8/HomeTask8/Figures.cs
+++ HomeTask8/HomeTask8/Figures.cs
@@ -8,8 +8,6 @@ namespace HomeTask8
 {
     public abstract class Figures
     {
-        //public abstract int Square { get; }
-        //public abstract int Perimetr { get; }
         public abstract double SquareCount();
         public abstract double PerimetrCount();
     }
@@ -20,17 +18,6 @@ namespace HomeTask8
         public double B { get; set; }
         public double C { get; set; }
         public double Height { get; set; }
-        //double square;
-        //double perimetr;
-        //public Triangle(int aInit, int bInit, int cInit, int heightInit) //constructor
-        //{
-        //    a = aInit;
-        //    b = bInit;
-        //    c = cInit;
-        //    height = heightInit;
-        //}
-        //public double Square { get; set; }
-        //public double Perimetr { get; set; }
         public override double SquareCount() => C * Height / 2;
 
         public override double PerimetrCount() => A + B + C;
@@ -41,15 +28,6 @@ namespace HomeTask8
     {
         public double X { get; set; }
         public double Y { get; set; }
-        //double square;
-        //double perimetr;
-        //public Rectangle(int xInit, int yInit) //constructor
-        //{
-        //    x = xInit;
-        //    y = yInit;
-        //}
-        //public int Square { get; set; }
-        //public int Perimetr { get; set; }
         public override double SquareCount() => X * Y;
         public override double PerimetrCount() => (X + Y) * 2;
 
@@ -57,10 +35,6 @@ namespace HomeTask8
     class Circle : Figures
     {
         public double R { get; set; }
-        //double square;
-        //double perimetr;
-        //public override int Square { get; }
-        //public override int Perimetr { get; }
         public override double SquareCount() => 3.14159 * R * R;
 
         public override double PerimetrCount() => 2 * 3.14159 * R;
